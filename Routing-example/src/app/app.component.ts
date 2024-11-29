3616331d178fcfd04a95b8b4904405e33a9312bb
dagz77 <20237877@s.ubaguio.edu>
2024-11-29 13:30:17 +0800

    nov-29-2024 / 1:45 PM

--- Routing-example/src/app/app.component.ts
+++ Routing-example/src/app/app.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterLink, RouterLinkActive,RouterModule,RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterModule,
+    ReactiveFormsModule,
+    CommonModule,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Routing-example';
 }
+
--- Routing-example/src/app/app.routes.ts
+++ Routing-example/src/app/app.routes.ts
@@ -1,13 +1,27 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { Component,NgModule } from '@angular/core';
 
 export const routes: Routes = [
     {path: '', component:HomeComponent},
     {path: 'abouts', component:AboutComponent},
     {path: 'sign-up', component:RegistrationComponent},
     {path: 'profile', component:UserProfileComponent},
+    {path: 'login', component:LoginComponent},
+    {path: 'service', component:ServicesComponent},
+    {path: 'footer', component:FooterComponent}
 ];
 
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
