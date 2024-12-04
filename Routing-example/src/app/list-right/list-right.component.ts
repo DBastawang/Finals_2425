a7490229585cf9ef553844b9df895b1139394bc0
dagz77 <20237877@s.ubaguio.edu>
2024-12-04 05:20:54 -0800

    date: 12-04-24 time: 21:20

--- Routing-example/src/app/list-right/list-right.component.ts
+++ Routing-example/src/app/list-right/list-right.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list-right',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './list-right.component.html',
-  styleUrl: './list-right.component.css'
+  styleUrls: ['./list-right.component.css'],
 })
 export class ListRightComponent {
+  ListRightComponent = {
+    id: 0,
+    name: '',
+    description: '',
+    category: '',
+    cost: 0,
+  };
 
+  saveService() {
+    if (
+      this.ListRightComponent.id &&
+      this.ListRightComponent.name &&
+      this.ListRightComponent.description &&
+      this.ListRightComponent.category &&
+      this.ListRightComponent.cost
+    ) {
+      alert('Details saved successfully!');
+      console.log(this.ListRightComponent);
+    } else {
+      alert('Please fill in all fields.');
+    }
+  }
+
+  cancelOperation() {
+    if (confirm('Are you sure you want to cancel? Unsaved changes will be lost.')) {
+      this.ListRightComponent = {
+        id: 0,
+        name: '',
+        description: '',
+        category: '',
+        cost: 0,
+      };
+    }
+  }
 }
